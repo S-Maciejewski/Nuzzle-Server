ea6889ebff72052064b7f8c1189d1254c3dd9d00
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2019-12-14 00:39:21 +0100

    Test request (SSL)

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+// const https = require('https')
 
 const app = express();
 const port = 3000;
+// const httpsOptions = {
+    // key: fs.readFileSync('./key.pem'),
+    // cert: fs.readFileSync('./cert.pem')
+// }
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// For demo purposes only
+// For demo purposes only, tokens should be JWT with HS256 encryption
+// Users should have id, login (email), name, surname, password, address, phone number etc.
 const dummyUsers = [
     {
         login: 'jan@test.com',
@@ -25,11 +31,17 @@ const dummyUsers = [
         password: 'bach',
         token: 'bachtoken'
     },
+    {
+        login: 'maciejewski.torun@gmail.com',
+        password: 'a',
+        token: 'testToken'
+    },
 ]
 
 async function setupServer() {
     // Some DBMS connection?
     app.listen(port, () => console.log(`Nuzzle server listening on port ${port}`));
+    // https.createServer(httpsOptions, app).listen(port, () => console.log(`Nuzzle server listening on port ${port}`));
 }
 
 function getAuthorization(login, password) {
@@ -39,11 +51,17 @@ function getAuthorization(login, password) {
 }
 
 app.post('/login', (req, res) => {
+    console.log('got login request, user:', req.body)
     try {
         res.send(getAuthorization(req.body.login, req.body.password));
     } catch (err) {
         console.log('Login failed with error ', err);
     }
 });
+
+app.get('/test', (req, res) => {
+    console.log('got test request');
+    res.send({ success: true })
+})
 
 setupServer();
