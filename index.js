b9b7e2e8b237ceceba37d71a817dd90a40d8d1a3
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2019-12-14 03:42:50 +0100

    Post offer WIP, deployment fixes

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const queries = require('./queries.js');
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const cors = require('cors');
+const path = require('path');
+const dbPath = path.resolve(__dirname, 'NuzzleDB.sqlite3'); // Useful in case of path problems when deployed to heroku
 
 const app = express();
 const port = 3000;
 
 var db;
 var authData;
+var tokenMap;
 
 app.use(cors());
 app.use(express.json());
@@ -15,13 +18,17 @@ app.use(express.urlencoded({ extended: true }));
 
 
 function connect() {
-    db = new sqlite3.Database('./NuzzleDB.sqlite3', (err) => {
+    db = new sqlite3.Database(dbPath, (err) => {
         console.log('Connection to SQLite failed, perhaps database file is missing?', err);
     });
 
     // For demo purposes only, tokens should be JWT with HS256 encryption
     db.all(queries.authData, (err, data) => {
         authData = data;
+    });
+
+    db.all(queries.tokenMap, (err, data) => {
+        tokenMap = data;
     });
 }
 
@@ -36,6 +43,10 @@ function getAuthorization(login, password) {
     return { success: false, message: 'Błąd logowania - sprawdź swój login i hasło' };
 }
 
+function getUserID(token) {
+    return tokenMap.filter(entry => entry.Token === token)[0].UserID;
+}
+
 app.post('/login', (req, res) => {
     try {
         res.send(getAuthorization(req.body.login, req.body.password));
@@ -43,6 +54,12 @@ app.post('/login', (req, res) => {
         console.log('Login failed with error ', err);
     }
 });
+
+app.post('/offer', (req, res) => {
+    console.log(getUserID(req.headers.authorization));
+
+    //TODO Post offer
+})
 
 app.get('/test', (req, res) => {
     res.send({ success: true })
--- queries.js
+++ queries.js
@@ -1,3 +1,10 @@
 module.exports = {
     authData: 'select Login, Password, Token from F_Users join D_Password DP on F_Users.ID = DP.UserID join D_TokenMap DT on F_Users.ID = DT.UserID',
+
+    tokenMap: 'select * from D_TokenMap',
+
+    postOffer: function (ownerID, params) {
+        return `insert into F_Offers values (random() % 10000, ${ownerID}, ${params.typeID}, ${new Date(Date.now()).toLocaleString('en-GB')},
+        ${params.LatitudePos}, ${params.LongitudePos}, ${params.AddressID}, ${params.Price}, '${params.Description}', '${params.ImageURL}');`;
+    },
 } 
